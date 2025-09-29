8411052ac205bfc52f3f7b1420b184be591eb824
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-29 17:11:45 +0600

    added Tokamak and TokamakShim for SwiftUI-like ux and API for web

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "MovieTracker",
+    dependencies: [
+        .package(url: "https://github.com/TokamakUI/Tokamak.git", from: "0.13.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MovieTracker"
+            name: "MovieTracker",
+            dependencies: [
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]
         ),
     ]
 )
